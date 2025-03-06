3c46c2ff179849b135902746cae58be96b91e31a
toki513 <tokitahmidkhan@gmail.com>
2025-03-06 11:21:17 +0600

    Add project files.

---
+++ Models/Domain/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿using DailyBlog.Models.Domain;
+
+namespace DailyBlog.Models.NewFolder
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public string Author { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+        public bool Visible     { get; set; }
+        public ICollection <Tag> Tags { get; set; }
+    }
+}
